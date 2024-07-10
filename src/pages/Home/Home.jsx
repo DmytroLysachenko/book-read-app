c425f96966996b8eead592f2ed0a7d2254a1ac57
Dimka863 <dlysachenko98@gmail.com>
2024-07-10 22:15:36 +0200

    added chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,29 @@ import { GoalsCounter } from '../../components/GoalsCounter/GoalsCounter';
 import { StartTrainingForm } from '../../components/StartTrainingForm/StartTrainingForm';
 import { selectCurrentPlanning } from '../../redux/books/selectors';
 import { PlanningBookList } from '../../components/PlanningBookList/PlanningBookList';
+import { LineChart } from '@mui/x-charts';
 
 export const Home = () => {
   const planning = useSelector(selectCurrentPlanning);
   return (
     <div className="h-noHeaderMob overflow-y-scroll py-5">
-      <GoalsCounter planning={planning} />
-      <PlanningBookList books={planning.books} />
+      {planning && <GoalsCounter planning={planning} />}
+      {planning && <PlanningBookList books={planning.books} />}
+      {planning && (
+        <LineChart
+          xAxis={[
+            { scaleType: 'point', data: planning.stats.map((x) => x.time) },
+          ]}
+          series={[
+            {
+              data: planning.stats.map((x) => x.pagesCount),
+            },
+          ]}
+          width={500}
+          height={300}
+          colors={{}}
+        />
+      )}
     </div>
   );
 };
