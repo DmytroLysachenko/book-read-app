032e285f60ca29caecf31239cd5d230435fe7fd1
Dimka863 <dlysachenko98@gmail.com>
2024-07-11 19:04:03 +0200

    year countdown completed

--- src/components/YearCountDown/YearCountDown.jsx
+++ src/components/YearCountDown/YearCountDown.jsx
@@ -24,23 +24,36 @@ export const YearCountDown = () => {
     return 59 - today.getSeconds();
   }, [today]);
   return (
-    <div className="flex gap-7">
-      <div className="flex flex-col">
-        <span>{days}</span>
-        <span>days</span>
+    <>
+      <h2 className="block w-70 mx-auto text-center text-sm text-gray_text">
+        End of the year countdown
+      </h2>
+      <div className="flex gap-7 bg-white px-8 py-2 mx-auto mt-2 w-70 shadow-counter">
+        <div className="flex flex-col items-center justify-center ">
+          <span className="block w-full font-OpenSans text-2xl text-center font-bold text-bgOverlay relative after:content-[':'] after:font-serif after:absolute after:-right-3.5 after:text-2xl after:font-bold after:text-bgOverlay">
+            {days}
+          </span>
+          <span className="text-10px text-gray_text">DAYS</span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <span className="block w-full font-OpenSans text-2xl text-center font-bold text-bgOverlay relative after:content-[':'] after:font-serif after:absolute after:-right-3.5 after:text-2xl after:font-bold after:text-bgOverlay">
+            {hours.toString().padStart(2, '0')}
+          </span>
+          <span className="text-10px text-gray_text">HRS</span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <span className="block w-full font-OpenSans text-2xl text-center font-bold text-bgOverlay relative after:content-[':'] after:font-serif after:absolute after:-right-3.5 after:text-2xl after:font-bold after:text-bgOverlay">
+            {minutes.toString().padStart(2, '0')}
+          </span>
+          <span className="text-10px text-gray_text">MINS</span>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <span className="font-OpenSans text-2xl font-bold text-bgOverlay">
+            {seconds.toString().padStart(2, '0')}
+          </span>
+          <span className="text-10px text-gray_text">SECS</span>
+        </div>
       </div>
-      <div className="flex flex-col">
-        <span>{hours}</span>
-        <span>hours</span>
-      </div>
-      <div className="flex flex-col">
-        <span>{minutes}</span>
-        <span>minutes</span>
-      </div>
-      <div className="flex flex-col">
-        <span>{seconds}</span>
-        <span>seconds</span>
-      </div>
-    </div>
+    </>
   );
 };
